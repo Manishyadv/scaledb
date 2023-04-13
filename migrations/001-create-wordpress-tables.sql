41f8626b26c3d230a0d4b321ed6ce7e697d19317
Manish Yadav <manishyadv@yandex.com>
2023-04-14 04:05:23 +0530

    Create 001-create-wordpress-tables.sql

---
+++ migrations/001-create-wordpress-tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE wp_users (
+  ID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  user_login varchar(60) NOT NULL DEFAULT '',
+  user_pass varchar(255) NOT NULL DEFAULT '',
+  user_nicename varchar(50) NOT NULL DEFAULT '',
+  user_email varchar(100) NOT NULL DEFAULT '',
+  user_url varchar(100) NOT NULL DEFAULT '',
+  user_registered datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  user_activation_key varchar(255) NOT NULL DEFAULT '',
+  user_status int(11) NOT NULL DEFAULT '0',
+  display_name varchar(250) NOT NULL DEFAULT '',
+  PRIMARY KEY (ID),
+  KEY user_login_key (user_login),
+  KEY user_nicename (user_nicename),
+  KEY user_email (user_email)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+
+CREATE TABLE wp_usermeta (
+  umeta_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  user_id bigint(20) unsigned NOT NULL DEFAULT '0',
+  meta_key varchar(255) DEFAULT NULL,
+  meta_value longtext,
+  PRIMARY KEY (umeta_id),
+  KEY user_id (user_id),
+  KEY meta_key (meta_key(191))
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci;
+
+-- Add other table schemas as needed.
